66930547828ead9015754881996d6a8cd082e045
Mustafa Mujahid <103353575+mustafa2mujahid@users.noreply.github.com>
2024-07-17 02:40:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def local_css(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 add_bg_from_local('bg.png')
-local_css("style/style.css")
+local_css("style.css")
 
 # Sidebar: If using streamlit_option_menu
 with st.sidebar:
