16716f572d0ebab9b65ae1c9b150ba627a3c7b7a
Mustafa Mujahid <103353575+mustafa2mujahid@users.noreply.github.com>
2024-07-17 04:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ with st.sidebar:
             st.empty()
     
     choose = option_menu(
-                        "Mustafa Mujahid"
+                        "Mustafa Mujahid",
                         ["About Me","Resume", "Experience", "Technical Skills", "Education", "Projects", "Competitions", "Volunteering", "Blog"],
                          icons=['person fill', 'globe', 'clock history', 'tools', 'book half', 'clipboard', 'trophy fill', 'heart', 'pencil square', 'image', 'paperclip', 'star fill', 'envelope'],
                          menu_icon="mortarboard", 
